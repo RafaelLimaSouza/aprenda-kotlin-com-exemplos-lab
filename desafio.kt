4fe2f70cf2e31cd394f407238549434395753aec
Rafael Lima <63744837+RafaelLimaSouza@users.noreply.github.com>
2023-04-01 21:02:59 -0300

    create todo and refactor classes
    Create todo following task description

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,43 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val name: String, val telefone: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(usuario: List<Usuario>) {
+        for (aluno in usuario){
+            inscritos.filter{ it -> it.telefone == aluno.telefone}.also { response ->
+                with(aluno){
+                    if(response.isEmpty()) {
+                        inscritos.add(aluno)
+                        println("Aluno $name matriculado com sucesso!")
+                    } else {
+                        println("Aluno $name ja esta matriculado!")
+                    }
+                }
+            }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudoBasicoKotlin = listOf(ConteudoEducacional("Types"),ConteudoEducacional("Null safety"))
+    val conteudoIntermediarioKotlin = listOf(ConteudoEducacional("Classes"),ConteudoEducacional("Classes Especiais"))
+    val conteudoAvancadoKotlin = listOf(ConteudoEducacional("Funcoes"),ConteudoEducacional("Escopo de Funcoes"))
+
+    val formacaoBasicaKotlin = Formacao("Kotlin", Nivel.BASICO, conteudoBasicoKotlin)
+    val formacaoIntermediariaKotlin = Formacao("Kotlin", Nivel.INTERMEDIARIO, conteudoIntermediarioKotlin)
+    val formacaoAvancadaKotlin = Formacao("Kotlin", Nivel.AVANCADO, conteudoAvancadoKotlin)
+
+    val usuariosNivelBasico = listOf(Usuario("Joao", "11999999999"))
+    val usuariosNivelIntermediario = listOf(
+        Usuario("Maria", "11000000000"), Usuario("Jose", "11888888888"), Usuario("Jose", "11888888888")
+    )
+
+    formacaoBasicaKotlin.matricular(usuariosNivelBasico)
+    formacaoIntermediariaKotlin.matricular(usuariosNivelIntermediario)
 }
